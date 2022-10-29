a15cc2c98ec4c755bdd877b3a9124e2a46396d64
Greg Petrites <gpetrites@ddaftech.com>
2022-10-29 08:15:38 -0400

    Rename types for rules

--- Fable.React-Hook-Form/Validation.fs
+++ Fable.React-Hook-Form/Validation.fs
@@ -4,31 +4,31 @@ open System.Text.RegularExpressions
 open Fable.Core.JS
 
 module Validation =
-    type IntRule = { value: int; message: string }
-    type DecimalRule = { value: decimal; message: string }
-    type PatternRule = { value: Regex; message: string }
-    type RuleValidateResult<'T> = 'T -> Result<'T,string>
+    type RuleIntProp = { value: int; message: string }
+    type RuleDecimalProp = { value: decimal; message: string }
+    type RulePatternProp = { value: Regex; message: string }
+    type RuleValidateResult<'T> = 'T -> Result<'T, string>
     type RuleValidateOption<'T> = 'T -> Option<string>
-    type RuleValidateAsyncResult<'T> = 'T -> Async<Result<'T,string>>
+    type RuleValidateAsyncResult<'T> = 'T -> Async<Result<'T, string>>
     type RuleValidatePromiseOption<'T> = 'T -> Promise<string option>
 
     type Rule<'T> =
         | Required of string
 
         | MaxLength of int * string
-        | [<CompiledName("maxLength")>] MaxLength' of IntRule
+        | [<CompiledName("maxLength")>] MaxLength' of RuleIntProp
 
         | MinLength of int * string
-        | [<CompiledName("maxLength")>] MinLength' of IntRule
+        | [<CompiledName("maxLength")>] MinLength' of RuleIntProp
 
         | Max of decimal * string
-        | [<CompiledName("max")>] Max' of DecimalRule
+        | [<CompiledName("max")>] Max' of RuleDecimalProp
 
         | Min of decimal * string
-        | [<CompiledName("mmin")>] Min' of DecimalRule
+        | [<CompiledName("mmin")>] Min' of RuleDecimalProp
 
         | Pattern of string * string
-        | [<CompiledName("pattern")>] Pattern' of PatternRule
+        | [<CompiledName("pattern")>] Pattern' of RulePatternProp
 
         | Validate of RuleValidateResult<'T>
         | [<CompiledName("validate")>] ValidateOption of RuleValidateOption<'T>
